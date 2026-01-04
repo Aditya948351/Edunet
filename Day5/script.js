566359a11779ee285b70975fbcadefe6ac8d70db
Aditya Patil <157286761+Aditya948351@users.noreply.github.com>
2026-01-04 17:27:43 +0530

    Added Most of the changes added COunter Stranger Things

--- Day5/script.js
+++ Day5/script.js
@@ -1,8 +1,39 @@
-let count = 0;
+let count = 1;
+let intervalId = null;
+const countDisplay = document.getElementById('count');
 
-while (count <= 10) {
-    document.writeln(count);
+const video = document.querySelector('video');
+
+
+function startCounter() {
+  if (intervalId !== null) return;
+  intervalId = setInterval(() => {
     count++;
-    const countDisplay = document.getElementById('count');
+    countDisplay.textContent = count;
+    if (count === 60) {
+      count = 0;
+      countDisplay.textContent = count;
+    }
+  }, 1000);
+  video.play();
 }
+function stopCounter() {
+  clearInterval(intervalId);
+  intervalId = null;
+  video.pause();
+}
+
+function resetCounter() {
+  clearInterval(intervalId);
+  intervalId = null;
+  count = 0;
+  countDisplay.textContent = count;
+  video.currentTime = 0;
+  video.pause();
+}
+document.getElementById('start').addEventListener('click', startCounter);
+
+document.getElementById('stop').addEventListener('click', stopCounter);
+document.getElementById('reset').addEventListener('click', resetCounter);
+
 
